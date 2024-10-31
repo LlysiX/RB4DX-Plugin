caac9975d21a1dc715e95d9af505387b5e5b7997
LysiX <smyler02@gmail.com>
2024-10-31 18:27:33 -0500

    initial gem colors work

---
+++ include/rb4/gemsmasher.h
@@ -0,0 +1,11 @@
+typedef struct _Color {
+    float r;
+    float g;
+    float b;
+    float a;
+} Color;
+
+typedef struct _RBGemSmasherCom {
+    char pad[32];
+    Color mColor;
+} RBGemSmasherCom;
--- source/main.c
+++ source/main.c
@@ -22,6 +22,7 @@
 #include "plugin_common.h"
 #include "DTAFuncs.h"
 #include "rb4/songmetadata.h"
+#include "rb4/gemsmasher.h"
 
 attr_public const char *g_pluginName = PLUGIN_NAME;
 attr_public const char *g_pluginDesc = "Plugin for loading Rock Band 4 Deluxe files, among other enhancements.";
@@ -217,6 +218,111 @@ void TscePadSetLightBar_hook(int handle, OrbisPadColor *inputColor) {
     return;
 }
 
+// Custom gem colors from RBVREnhanced, updated to set gems individually instead of all at once
+
+bool(*UpdateColors)(RBGemSmasherCom* thiscom);
+
+HOOK_INIT(UpdateColors);
+
+bool UpdateColors_hook(RBGemSmasherCom* thiscom) {
+
+    colorptr = &(thiscom->mColor);
+    bool r = HOOK_CONTINUE(UpdateColors, bool(*)(RBGemSmasherCom*), thiscom);
+
+    //final_printf("Smasher color detected!\n");
+    //final_printf("R: %f\n", thiscom->mColor.r);
+    //final_printf("G: %f\n", thiscom->mColor.g);
+    //final_printf("B: %f\n", thiscom->mColor.b);
+
+    char coltest[9];
+    sprintf(coltest, "%f", thiscom->mColor.r);
+
+    //green
+    if (strcmp(coltest, "0.130136") == 0) {
+        thiscom->mColor.r = 1.0;
+        thiscom->mColor.g = 0.1;
+        thiscom->mColor.b = 1.0;
+        thiscom->mColor.a = 1.0;
+    }
+    //red
+    if (strcmp(coltest, "0.896269") == 0) {
+        thiscom->mColor.r = 1.0;
+        thiscom->mColor.g = 0.5;
+        thiscom->mColor.b = 0.0;
+        thiscom->mColor.a = 1.0;
+    }
+    //yellow
+    if (strcmp(coltest, "0.791298") == 0) {
+        thiscom->mColor.r = 0.01;
+        thiscom->mColor.g = 0.01;
+        thiscom->mColor.b = 0.01;
+        thiscom->mColor.a = 1.0;
+    }
+    //blue
+    if (strcmp(coltest, "0.011612") == 0) {
+        thiscom->mColor.r = 1.0;
+        thiscom->mColor.g = 0.5;
+        thiscom->mColor.b = 0.0;
+        thiscom->mColor.a = 1.0;
+    }
+    //orange
+    if (strcmp(coltest, "0.745404") == 0) {
+        thiscom->mColor.r = 1.0;
+        thiscom->mColor.g = 0.1;
+        thiscom->mColor.b = 1.0;
+        thiscom->mColor.a = 1.0;
+    }
+
+    return r;
+}
+
+bool (*DoSetColor)(void* component, void* proppath, void* propinfo, Color* color, Color* toset, bool param_6);
+
+HOOK_INIT(DoSetColor);
+
+bool DoSetColor_hook(void* component, void* proppath, void* propinfo, Color* color, Color* toset, bool param_6) {
+    Color newcolorg;
+    newcolorg.r = 1.0;
+    newcolorg.g = 0.1;
+    newcolorg.b = 1.0;
+    newcolorg.a = 1.0;
+    Color newcolorr;
+    newcolorr.r = 1.0;
+    newcolorr.g = 0.5;
+    newcolorr.b = 0.0;
+    newcolorr.a = 1.0;
+    Color newcolory;
+    newcolory.r = 0.01;
+    newcolory.g = 0.01;
+    newcolory.b = 0.01;
+    newcolory.a = 1.0;
+    Color newcolorb;
+    newcolorb.r = 1.0;
+    newcolorb.g = 0.5;
+    newcolorb.b = 0.0;
+    newcolorb.a = 1.0;
+    Color newcoloro;
+    newcoloro.r = 1.0;
+    newcoloro.g = 0.1;
+    newcoloro.b = 1.0;
+    newcoloro.a = 1.0;
+    char coltest[9];
+    sprintf(coltest, "%f", toset->r);
+    if (strcmp(coltest, "0.130136") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolorg, param_6);
+    if (strcmp(coltest, "0.242281") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolorg, param_6);
+    //red
+    if (strcmp(coltest, "0.896269") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolorr, param_6);
+    //yellow
+    if (strcmp(coltest, "0.791298") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolory, param_6);
+    //blue
+    if (strcmp(coltest, "0.011612") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolorb, param_6);
+    if (strcmp(coltest, "0.004025") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcolorb, param_6);
+    //orange
+    if (strcmp(coltest, "0.745404") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcoloro, param_6);
+    if (strcmp(coltest, "0.814847") == 0) return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, &newcoloro, param_6);
+    return HOOK_CONTINUE(DoSetColor, bool(*)(void*, void*, void*, Color*, Color*, bool), component, proppath, propinfo, color, toset, param_6);
+}
+
 #define ADDR_OFFSET 0x00400000
 int32_t attr_public module_start(size_t argc, const void *args)
 {
@@ -253,6 +359,8 @@ int32_t attr_public module_start(size_t argc, const void *args)
     GetTitle = (void*)(procInfo.base_address + 0x00f28d20);
     SetMusicSpeed = (void*)(procInfo.base_address + 0x00a470e0);
     TscePadSetLightBar = (void*)(procInfo.base_address + 0x012450d0);
+    UpdateColors = (void*)(procInfo.base_address + 0x00f94a70);
+    DoSetColor = (void*)(procInfo.base_address + 0x001a7320);
 
     // apply all hooks
     InitDTAHooks();
@@ -260,6 +368,8 @@ int32_t attr_public module_start(size_t argc, const void *args)
     HOOK(GetTitle);
     HOOK(NewFile);
     HOOK(TscePadSetLightBar);
+    HOOK(UpdateColors);
+    HOOK(DoSetColor);
 
     return 0;
 }
